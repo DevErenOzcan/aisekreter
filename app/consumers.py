66a5a5208c5d88f307be0c2a1e16b59d16b9b599
Eren Özcan <dev.eren.ozcan@proton.me>
2025-02-21 20:43:55 +0300

    consumer tamamlandı

--- app/consumers.py
+++ app/consumers.py
@@ -3,8 +3,8 @@ import io
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.conf import settings
 import aiofiles
-import wave
-import numpy as np
+from pydub import AudioSegment
+import torchaudio
 
 
 class AudioConsumer(AsyncWebsocketConsumer):
@@ -12,54 +12,58 @@ class AudioConsumer(AsyncWebsocketConsumer):
         super().__init__(*args, **kwargs)
         self.meeting_id = None
         self.segment_count = 0
-        self.header = None
-        self.data = bytearray()
+        self.last_segment_end = None
+        self.audio_segment = AudioSegment.silent(duration=0)
+
 
     async def connect(self):
         self.meeting_id = self.scope['url_route']['kwargs']['meeting_id']
         await self.accept()
 
+
     async def receive(self, text_data=None, bytes_data=None):
         if bytes_data:
-            if self.header is None:
-                self.header = bytes_data[:44]
-                print("header received")
-                self.data = bytes_data[44:]
+            audio_stream = io.BytesIO(bytes_data)
+            new_segment = AudioSegment.from_file(audio_stream, format="wav")
+            self.audio_segment += new_segment
 
-            else:
-                self.data += bytes_data
             print("-" * 100)
-            print(f"Received {len(bytes_data)} bytes")
+            print(f"Received {len(bytes_data)} bytes and added to AudioSegment")
             await self.process_audio()
 
+
     async def process_audio(self):
-        audio_data = self.header + bytes(self.data)
-        audio_file = io.BytesIO(audio_data)
+        audio_buffer = io.BytesIO()
+        self.audio_segment.export(audio_buffer, format="wav")
+        audio_buffer.seek(0)
+        waveform, sample_rate = torchaudio.load(audio_buffer)
+
         diarization_result = settings.DIARIZATION_MODEL(
-            {'audio': audio_file, 'uri': f"meeting_{self.meeting_id}{self.segment_count}"})
+            {"uri": "memory_audio", "waveform": waveform, "sample_rate": sample_rate})
 
         segments = list(diarization_result.itertracks(yield_label=True))
         for i, (segment, seg_char, speaker) in enumerate(segments):
-            # sample rate 48.000 her sample başına düşen byte sayısı 2
-            start_byte = int(segment.start * 48000 * 2)
-            end_byte = int(segment.end * 48000 * 2)
-            segment_data = self.data[start_byte:end_byte]
-            print(f"{i}.segment size: {len(segment_data)} bytes")
+            # segment datasını ms cinsinden alıyorum o yüzden 1000
+            start_time = int(segment.start * 1000)
+            end_time = int(segment.end * 1000)
+            segment_data = self.audio_segment[start_time:end_time]
+            print(f"{i}.segment size: {len(segment_data)} ms")
 
             # segment 0.3 saniyeden büyükse ve son segment değilse işleme sokuyorum
-            if len(segment_data) > 30000 and i != len(segments) - 1:
-                await self.save_audio(f"meetings/{self.meeting_id}/{self.segment_count}.wav", self.header + segment_data)
-                self.data = self.data[end_byte:]
+            if len(segment_data) > 300 and i != len(segments) - 1:
+                await self.save_audio(f"meetings/{self.meeting_id}/{self.segment_count}.wav", segment_data)
+                self.audio_segment = self.audio_segment[end_time:]
                 self.segment_count += 1
                 print(f"meetings/{self.meeting_id}/{self.segment_count}.wav saved")
+
 
     async def save_audio(self, filename, data):
         directory = os.path.dirname(filename)
         if directory and not os.path.exists(directory):
             os.makedirs(directory, exist_ok=True)
 
-        async with aiofiles.open(filename, 'ab') as temp_file:
-            await temp_file.write(data)
+        data.export(filename, format="wav")
+
 
     async def disconnect(self, close_code):
         print("Connection closed.")
