2931116169f0c72bd159945ed5cee26df3bdcf44
Eren Özcan <dev.eren.ozcan@proton.me>
2025-03-12 16:36:01 +0300

    vad segmentasyon optimizasyonu

--- app/consumers.py
+++ app/consumers.py
@@ -25,7 +25,6 @@ class AudioConsumer(AsyncWebsocketConsumer):
         self.segment_count = 0
         self.buffer = bytearray()
         self.current_segment = bytearray()
-        self.not_speech_buffer = bytearray()
         self.is_speech_now = False
         self.not_speech_count = 0
 
@@ -38,56 +37,47 @@ class AudioConsumer(AsyncWebsocketConsumer):
             # bytes data da 44 byte lık header var. Bu header ı atıyorum
             bytes_data = bytes_data[44:]
 
-            # bytes data nın 960 a tam bölünen kısmını alıyorum. Kalan kısmı bir sonraki datada kullanmak için buffer a ekliyorum
+            # buffer da kalan data yı ekliyorum
             bytes_data = self.buffer + bytes_data
-            samples = bytes_data[:len(bytes_data) // 960 * 960]
-            self.buffer = bytes_data[len(samples):]
+
+            # bytes data nın 960 a tam bölünen kısmını alıyorum. Kalan kısmı bir sonraki datada kullanmak için buffer a ekliyorum
+            useable_data_len = int(len(bytes_data) // frame_size * frame_size)
+            samples = bytes_data[:useable_data_len]
+            self.buffer = bytes_data[useable_data_len:]
+
             for i in range(0, len(samples), frame_size):
                 frame_data = samples[i:i + frame_size]
                 is_speech = vad.is_speech(frame_data, sr)
 
                 if is_speech:
-                    if self.is_speech_now:
-                        self.current_segment += frame_data
-
-                    else:
-                        # sessizlik 30 frame den azsa mevcut segmenti bölmüyorum
-                        if self.not_speech_count < 30:
-                            self.current_segment += self.not_speech_buffer
-                            self.not_speech_buffer = bytearray()
-                            self.current_segment += frame_data
-
-                        else:
-                            # mevcut segment 2 sn den kısaysa kaydetmiyorum
-                            if len(self.current_segment) > sr * 4:
-                                # mevcut segmentin sonuna "not_speech_buffer" daki sessizlğin yarısını ekleyip segmenti kaydediyorum
-                                self.current_segment += self.not_speech_buffer[int(len(self.not_speech_buffer)/2):]
+                    if not self.is_speech_now:
+                        # sessizlik 24 frame den fazlaysa ve segment 3 sn den uzunsa mevcut segmenti bölüyorum
+                        if len(self.current_segment) > sr * 6:
+                            if self.not_speech_count > 10:
+                                # son segmentin son kısmındaki sessizliğin ilk yarısını o segmentte bırakıp diğer yarısını yeni segmente ekleyeceğim
+                                # böylelikle sesi tam olarak sessizliğin ortasından bölmüş oluyorum.
+                                not_speech_len = self.not_speech_count * frame_size
+                                not_speech_buffer = self.current_segment[-int(not_speech_len / 2):]
+                                self.current_segment = self.current_segment[:-int(not_speech_len / 2)]
 
                                 file_path = f"meetings/meet_{self.meeting_id}/segment_{self.segment_count}.wav"
                                 await self.save_audio(file_path, self.current_segment)
                                 self.segment_count += 1
 
                                 print("-" * 100)
-                                print(f"new segment saved: {len(self.current_segment) / 96000} sn")
+                                print(f"new segment saved: {len(self.current_segment) / (sr * 2)} sn")
 
                                 # mevcut segmenti temizleyip yeni segmentin başına kalan yarısını ekliyorum. Böylece ses parçaları arasında kesinti olmuyor
                                 self.current_segment = bytearray()
-                                self.current_segment += self.not_speech_buffer[:int(len(self.not_speech_buffer)/2)]
-                                self.not_speech_buffer = bytearray()
+                                self.current_segment = not_speech_buffer
 
-                                # son olarak yeni segmente "frame_data" yı ekliyorum
-                                self.current_segment += frame_data
-
-                            else:
-                                self.current_segment += self.not_speech_buffer
-                                self.not_speech_buffer = bytearray()
-                                self.current_segment += frame_data
+                        self.not_speech_count = 0
 
                 else:
                     self.not_speech_count += 1
-                    self.not_speech_buffer += frame_data
 
                 self.is_speech_now = is_speech
+                self.current_segment += frame_data
 
 
     async def save_audio(self, filename, data):
@@ -102,4 +92,9 @@ class AudioConsumer(AsyncWebsocketConsumer):
             wav_file.writeframes(data)
 
     async def disconnect(self, close_code):
+        file_path = f"meetings/meet_{self.meeting_id}/segment_{self.segment_count}.wav"
+        await self.save_audio(file_path, self.current_segment)
+
+        print("-" * 100)
+        print(f"new segment saved: {len(self.current_segment) / (sr * 2)} sn")
         print("Connection closed.")
